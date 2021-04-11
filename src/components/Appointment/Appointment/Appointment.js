5be3cc24ce03ee7ba9bc05eeccdca1af828da490
niazmurshedsiam <niazmurshed16@gmail.com>
2021-04-11 23:21:37 +0600

    Create Appointment header and react calendar

--- src/components/Appointment/Appointment/Appointment.js
+++ src/components/Appointment/Appointment/Appointment.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import NavBar from '../../Shared/NavBar/NavBar';
+import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
 
 const Appointment = () => {
+    const [selectedDate,setSelectedDate] = useState(new Date());
+    const handleDateChange = date =>{
+        setSelectedDate(date);
+    }
     return (
         <div>
             <NavBar></NavBar>
+            <AppointmentHeader handleDateChange={handleDateChange}></AppointmentHeader>
             <Footer></Footer>
         </div>
     );
